6cc6a1722aacd42bad93faa94a18601db5b4f682
Gilian <35449373+GilianJuice@users.noreply.github.com>
2024-03-25 13:37:23 +0100

    Branches improvements (#184)
    * Added support for styled output changes in merge.
    
    * Working on adding more caching to merging of branches, to make it perform a little better.
    
    * Removed limit that was added for testing
    
    * Added functionality to log the progress of a branch merge in the database, so you can kind of see how far we are.

---
+++ WiserTaskScheduler/WiserTaskScheduler/Modules/Branches/Models/BranchMergeFileCacheModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WiserTaskScheduler.Modules.Branches.Models;
+
+public class BranchMergeFileCacheModel
+{
+    public ulong Id { get; set; }
+    public ulong? ItemId { get; set; }
+    public ulong? LinkId { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ WiserTaskScheduler/WiserTaskScheduler/Modules/Branches/Models/BranchMergeLinkCacheModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WiserTaskScheduler.Modules.Branches.Models;
+
+public class BranchMergeLinkCacheModel
+{
+    public ulong Id { get; set; }
+    public ulong? ItemId { get; set; }
+    public ulong? DestinationItemId { get; set; }
+    public int? Type { get; set; }
+    public bool IsDeleted { get; set; }
+}
