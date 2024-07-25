c8e87db00d0e5183acaab2c2363eebf0936e9fa4
Mike van Mourik <130973326+MikeVanMourik@users.noreply.github.com>
2024-07-25 14:00:12 +0200

    add the SkipAction OnlyWithValue (#207)
    * add the SkipAction OnlyWithValue
    
    * add extra check

--- WiserTaskScheduler/WiserTaskScheduler/Core/Services/ConfigurationsService.cs
+++ WiserTaskScheduler/WiserTaskScheduler/Core/Services/ConfigurationsService.cs
@@ -129,7 +129,9 @@ namespace WiserTaskScheduler.Core.Services
                 configuration.GenerateCommunicationGroup,
                 configuration.GenerateCommunications,
                 configuration.DocumentStoreReadersGroup,
-                configuration.DocumentStoreReader
+                configuration.DocumentStoreReader,
+                configuration.SlackMessageGroup,
+                configuration.SlackMessages
             };
 
             var allActions = new List<ActionModel>();
@@ -304,7 +306,42 @@ namespace WiserTaskScheduler.Core.Services
                     return true;
                 }
             }
+            
+            if (!String.IsNullOrWhiteSpace(action.OnlyWithValue))
+            {
+                var parts = action.OnlyWithValue.Split(",");
 
+                try
+                {
+                    var defaultValue = String.Empty;
+                    var defaultValueIndex = parts[0].IndexOf('?');
+                    if (defaultValueIndex >= 0)
+                    {
+                        defaultValue = parts[0][defaultValueIndex..];
+                        parts[0] = parts[0][..defaultValueIndex];
+                    }
+                    
+                    var state = ReplacementHelper.GetValue($"{parts[0]}{defaultValue}", ReplacementHelper.EmptyRows, resultSets, false);
+
+                    if (parts.Length < 2)
+                    {
+                        await logService.LogInformation(logger, LogScopes.RunStartAndStop, LogSettings, $"Skipped action because no value was provided to check state against (did you forget a comma?).", configurationServiceName, action.TimeId, action.Order);
+                        return true;
+                    }
+                    
+                    if (state != parts[1])
+                    {
+                        await logService.LogInformation(logger, LogScopes.RunStartAndStop, LogSettings, $"Skipped action because success state was '{state}' and not {parts[1]}.", configurationServiceName, action.TimeId, action.Order);
+                        return true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    await logService.LogError(logger, LogScopes.RunStartAndStop, LogSettings, $"Failed to validate action value state, skipping action. Exception: {e}", configurationServiceName, action.TimeId, action.Order);
+                    return true;
+                }
+            }
+            
             return false;
         }
     }
